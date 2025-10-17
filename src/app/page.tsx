0baf5d09823dc4665d3d9eb91b714c5e377f49d9
sspenterprisescontact-a11y <sspenterprises.contact@gmail.com>
2025-10-17 14:36:53 +0530

    [Change]-To Change the coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,26 +67,20 @@ import React from "react";
 
 const ComingSoon = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 relative ">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 relative">
+     
       <img
         src="../images/logo.png"
         alt="Logo"
-        className="
-    absolute top-5 lg:left-190
-    left-60 -translate-x-1/2   /* default: centered on mobile */
-    w-[110px] sm:w-[130px]  
-    md:left-95    /* mobile width 130px, desktop 180px */
-    sm:left-10 sm:translate-x-0   /* desktop: move to left and remove centering */
-    h-auto animate-float
-  "
-      />  
+        className="absolute top-5 left-1/2 transform -translate-x-1/2 w-[50px] sm:w-[70px] h-auto"
+      />
 
       {/* Main Content */}
       <div className="relative mt-36">
         <img
           src="../images/Frame 52757.png"
           alt="Walking Illustration"
-          className="w-auto h-[150px] mt-35 animate-skate absolute top-[-120px] z-1"
+          className="w-auto h-auto mt-20 animate-skate absolute top-[-120px] z-1"
         />
         <img
           src="../images/Frame 1000000879.png"
@@ -96,10 +90,10 @@ const ComingSoon = () => {
       </div>
 
       <div className="text-center rounded-md p-2">
-        <h2 className="text-3xl sm:text-6xl md:text-6xl lg:text-[50px] font-[700] text-[#9B7B2D] mb-5">
+        <h2 className="text-3xl sm:text-6xl md:text-6xl lg:text-[75px] font-[700] text-[#9B7B2D] mb-5">
           COMING SOON!!
         </h2>
-        <p className="font-medium text-[17.25px] text-[#666666] leading-[130%] tracking-[0.05em] text-center">
+        <p className="font-medium text-[19.25px] text-[#666666] leading-[130%] tracking-[0.05em] text-center">
           We're working hard to finish the website and we'll be ready <br />
           to launch after this brief today.
         </p>
@@ -107,47 +101,15 @@ const ComingSoon = () => {
 
       <div className="mt-10 overflow-hidden relative w-full">
         <div className="flex animate-scroll gap-0">
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 1"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 2"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 3"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg4.png"
-            alt="Gallery 4"
-            className="rounded-lg w-100"
-          />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 1" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 2" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 3" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg4.png" alt="Gallery 4" className="rounded-lg w-100" />
 
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 1"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 2"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg3.png"
-            alt="Gallery 3"
-            className="rounded-lg w-100"
-          />
-          <img
-            src="../images/Comingsoonimg4.png"
-            alt="Gallery 4"
-            className="rounded-lg w-100"
-          />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 1" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 2" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg3.png" alt="Gallery 3" className="rounded-lg w-100" />
+          <img src="../images/Comingsoonimg4.png" alt="Gallery 4" className="rounded-lg w-100" />
         </div>
       </div>
     </div>
